eb4e57fb9155bb7dfcd54dbfef6af1049ea36821
HannahCC <408559221@qq.com>
2015-07-15 21:17:01 +0800

    merge notice display and notice detail display

--- QianXun/notice/db/notice.py
+++ QianXun/notice/db/notice.py
@@ -1,7 +1,7 @@
 __author__ = 'Hannah'
 from QianXun.notice.models import CanteenNotice, SchoolNotice
 from utils.Pagination import get_paginator
-from QianXun.notice.beans import NoticeBean, CanteenNoticeDetailBean, SchoolNoticeDetailBean
+from QianXun.notice.beans import CanteenNoticeDetailBean, SchoolNoticeDetailBean
 
 
 def get_canteen_notice_bean_list_bycanteen(canteen_model, pagination_dict):
@@ -9,7 +9,7 @@ def get_canteen_notice_bean_list_bycanteen(canteen_model, pagination_dict):
     canteen_notice_model_list = CanteenNotice.objects.filter(canteen=canteen_model).order_by('-update_time')[paginator[0]:paginator[1]]
     canteen_notice_bean_list = []
     for canteen_notice_model in canteen_notice_model_list:
-        canteen_notice_bean = NoticeBean(canteen_notice_model)
+        canteen_notice_bean = CanteenNoticeDetailBean(canteen_notice_model)
         canteen_notice_bean_list.append(canteen_notice_bean)
     return canteen_notice_bean_list
 
